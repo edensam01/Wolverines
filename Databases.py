3fa70ab7de680a0bcdde31e454946c78cd8b6e69
aishanic18 <95302648+aishanic18@users.noreply.github.com>
2023-04-16 22:39:22 -0400

    Update Databases.py

--- Databases.py
+++ Databases.py
@@ -9,9 +9,17 @@ for row in data:
 for row in data:
     cur.execute("INSERT INTO SEO_data (date, traffic) VALUES (?, ?)", row)
 
-cur.execute('SELECT MIN(date) FROM Stock_SEO')
-result = cur.fetchone()
-min_value = result[0] 
+cur.execute('SELECT COUNT(*) as row_count FROM Stock_SEO')
+row = cur.fetchone()
+row_count = row[0]
+if (row_count == 0):
+    cur.execute('SELECT date()')
+    min = cur.fetchtone()
+    min_value = min[0]
+else:
+    cur.execute('SELECT MIN(date) FROM Stock_SEO')
+    result = cur.fetchone()
+    min_value = result[0] 
 
 conn.commit()
 conn.close()
