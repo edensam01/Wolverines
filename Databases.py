a32f255903c36cde77891accabf8130c18f55b5f
edensam01 <edensam@umich.edu>
2023-04-16 22:23:16 -0400

    data

--- Databases.py
+++ Databases.py
@@ -9,5 +9,9 @@ for row in data:
 for row in data:
     cur.execute("INSERT INTO SEO_data (date, traffic) VALUES (?, ?)", row)
 
+cur.execute('SELECT MIN(date) FROM Stock_SEO')
+result = cur.fetchone()
+min_value = result[0] 
+
 conn.commit()
 conn.close()
