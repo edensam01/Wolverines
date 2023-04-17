9d3b66231381bba440962eda578f121355c80f68
edensam01 <edensam@umich.edu>
2023-04-16 21:51:33 -0400

    Databases

--- Databases.py
+++ Databases.py
@@ -8,6 +8,7 @@ cur.execute('DROP TABLE IF EXISTS Stock')
 cur.execute('DROP TABLE IF EXISTS SEO_data')
 cur.execute('CREATE TABLE Stock (date DATE PRIMARY KEY, price REAL)')
 cur.execute('CREATE TABLE SEO_data (date DATE PRIMARY KEY, traffic REAL)')
+cur.execute('CREATE TABLE Stock_SEO AS SELECT Stock.date, Stock.price, SEO_data.traffic FROM Stock JOIN SEO_data ON Stock.date = SEO_data.date')
 
 conn.commit()
 conn.close()
